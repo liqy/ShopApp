1358bf2f79758eedabd18556d71cb6165904d18f
liqingyi <liqingyi@outlook.com>
2017-02-09 09:21:41 +0800

    Activity 基类

---
+++ app/src/main/java/com/bwie/shopapp/BaseActivity.java
@@ -0,0 +1,12 @@
+package com.bwie.shopapp;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * 1. 类的用途
+ * 2. @author：liqingyi
+ * 3. @date：2017/2/9 09:18
+ */
+
+public class BaseActivity extends AppCompatActivity {
+}
