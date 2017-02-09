bbafc7e271fbc1befe794ebc662f1be048972e7d
liqingyi <liqingyi@outlook.com>
2017-02-09 15:49:49 +0800

    分支

--- app/src/main/java/com/bwie/shopapp/fragment/ShopFragment.java
+++ app/src/main/java/com/bwie/shopapp/fragment/ShopFragment.java
@@ -1,4 +1,4 @@
-package com.bwie.shopapp;
+package com.bwie.shopapp.fragment;
 
 
 import android.os.Bundle;
@@ -6,6 +6,8 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.bwie.shopapp.R;
 
 
 /**
