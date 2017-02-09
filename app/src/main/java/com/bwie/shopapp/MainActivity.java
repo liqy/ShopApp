191114e28f5b1fa5fa758acc29261db3ecbe3b2d
liqingyi <liqingyi@outlook.com>
2017-02-09 09:23:09 +0800

    Activity 基类

--- app/src/main/java/com/bwie/shopapp/MainActivity.java
+++ app/src/main/java/com/bwie/shopapp/MainActivity.java
@@ -1,8 +1,9 @@
 package com.bwie.shopapp;
 
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
-public class MainActivity extends BaseActivity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
